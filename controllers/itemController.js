0d61130a6c97d764fc6eedd92ba26fd201c830d4
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-05-30 11:49:56 +0530

    add: item controller placeholder functions

---
+++ controllers/itemController.js
@@ -0,0 +1,33 @@
+const Item = require('../models/item');
+const { body, validationResult } = require('express-validator');
+const asyncHandler = require('express-async-handler');
+const debug = require('debug')('item:');
+
+exports.index = asyncHandler(async (req, res, next) => {
+  res.send('TODO: GET Index');
+});
+
+exports.item_list = asyncHandler(async (req, res, next) => {
+  res.send('TODO: GET Item List');
+});
+exports.item_detail = asyncHandler(async (req, res, next) => {
+  res.send('TODO: GET Item Detail');
+});
+exports.item_create_get = asyncHandler(async (req, res, next) => {
+  res.send('TODO: GET Item Create');
+});
+exports.item_create_post = asyncHandler(async (req, res, next) => {
+  res.send('TODO: POST Item Create');
+});
+exports.item_update_get = asyncHandler(async (req, res, next) => {
+  res.send('TODO: GET Item Update');
+});
+exports.item_update_post = asyncHandler(async (req, res, next) => {
+  res.send('TODO: POST Item Update');
+});
+exports.item_delete_get = asyncHandler(async (req, res, next) => {
+  res.send('TODO: GET Item Delete');
+});
+exports.item_delete_post = asyncHandler(async (req, res, next) => {
+  res.send('TODO: POST Item Delete');
+});
