e662122afc6d7a62c341cabc14ad55c6be89e8c6
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-05-30 17:23:55 +0530

    add: category_update controller get/post

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -47,12 +47,37 @@ exports.category_create_post = [
 ];
 
 exports.category_update_get = asyncHandler(async (req, res, next) => {
-  res.send('TODO: GET Catgory update');
+  const category = await Category.findById(req.params.id).exec();
+  if (category === null) {
+    const err = new Error('Category not found!');
+    err.status = 404;
+    next(err);
+  }
+  res.render('category_create', { title: 'Update Category', category });
 });
 
-exports.category_update_post = asyncHandler(async (req, res, next) => {
-  res.send('TODO: POST Catgory update');
-});
+exports.category_update_post = [
+  body('name', 'Name too small').trim().isLength({ min: 2 }).escape(),
+  body('description', 'Description too small').trim().isLength({ min: 10 }).escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+      _id: req.params.id,
+    });
+    if (!errors.isEmpty()) {
+      res.render('category_create', {
+        title: 'Update Category',
+        category,
+        errors: errors.array(),
+      });
+    } else {
+      const updatedCategory = await Category.findByIdAndUpdate(req.params.id, category, {});
+      res.redirect(updatedCategory.url);
+    }
+  }),
+];
 
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
   res.send('TODO: GET Catgory delete');
