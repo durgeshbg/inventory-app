be27d032c0ae1dbfd276380ef0da33a3f427eac3
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2024-05-30 15:17:08 +0530

    add: category_detail controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 const Category = require('../models/category');
+const Item = require('../models/item');
 const { body, validationResult } = require('express-validator');
 const asyncHandler = require('express-async-handler');
 const debug = require('debug')('item:');
@@ -7,9 +8,18 @@ exports.category_list = asyncHandler(async (req, res, next) => {
   const categories = await Category.find({}).exec();
   res.render('category_list', { title: 'Categories List', categories });
 });
+
 exports.category_detail = asyncHandler(async (req, res, next) => {
-  res.send('TODO: GET Catgory detail');
+  const category = await Category.findById(req.params.id).exec();
+  const items = await Item.find({ category: req.params.id }).exec();
+  if (category === null) {
+    const err = new Error('Category not found!');
+    err.status = 404;
+    next(err);
+  }
+  res.render('category_detail', { title: 'Category Detail', category, items });
 });
+
 exports.category_create_get = asyncHandler(async (req, res, next) => {
   res.send('TODO: GET Catgory create');
 });
